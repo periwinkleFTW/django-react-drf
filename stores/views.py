6d9fc7e9f11422f96aa01df4a97d8cc1a25088d5
periwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2021-04-24 19:59:56 -0600

    sotre serializer and listapiview

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import PizzeriaListSerializer
+from .models import Pizzeria
+
+
+class PizzeriaListAPIView(generics.ListAPIView):
+    queryset = Pizzeria.objects.all()
+    serializer_class = PizzeriaListSerializer
+
